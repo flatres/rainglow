66bb06919994be0e6632d16c5a7fba4d7807919d
SD Flatres <sdf@marlboroughcollege.org>
2025-12-22 08:53:00 +0000

    Get Weather From Apis in Cron

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -3,14 +3,14 @@
     <div class="row q-col-gutter-md justify-start content-start items-start col-12 q-pr-sm q-pt-md items-stretch">
       <div class='full-width'>
         <div class="full-width text-center q-mt-xl">
-          <q-img
+          <!-- <q-img
             @click="$router.push('/')"
             src="~assets/logos/v3/smallWhite.png"
             spinner-color="white"
             fit="fill"
             style="heightx: 48px; max-width: 190px"
             class="q-mr-md qx-my-md"
-          />
+          /> -->
         </div>
         <h1 class="full-width text-bold text-grey-7 q-mt-none text-center" style="font-size:25px">Nothing here</h1>
       </div>
